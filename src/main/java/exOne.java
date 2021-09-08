b3e79ea5d541e8ac5490cf967ed101b9dbd474a1
EPro30 <82483823+EPro30@users.noreply.github.com>
2021-09-08 11:45:41 -0400

    Update exOne.java

--- src/main/java/exOne.java
+++ src/main/java/exOne.java
@@ -4,8 +4,8 @@ public class exOne {
     {
         System.out.println("What is you name?");
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        //String name = sc.nextLine();
+        //String name = sc.next();
+        String name = sc.nextLine();
         System.out.println("Hello, " + name + ", nice to meet you!");
     }
 }
